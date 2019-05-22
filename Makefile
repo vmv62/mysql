686306f802a1ec294c2b3a7f5a7989b70a4f0f75
vmv <vmv@nxt.ru>
2019-05-22 08:57:04 +0300

    Файл автосборки

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: mysql_cli.c
+			gcc mysql_cli.c -o mysql -I/usr/include/mysql -L/usr/lib/i386-linux-gnu  -lmariadbclient -lpthread -lz -lm -ldl
+clear:
+		rm -f mysql
--- mysql_cli.c
+++ mysql_cli.c
@@ -3,18 +3,18 @@
 #include <mysql.h>
 #include "mysql_cli.h"
 #include <string.h>
-/*
+
 int main(int argc, char **argv){
 	float mmm[4] = {23.3, 56, 78.4};
 
 	DBDAT *db_p;
 	db_p = (DBDAT *)malloc(sizeof(DBDAT));
 
-	strcpy(db_p->user, "root");
+	strcpy(db_p->user, "vmv");
 	strcpy(db_p->passwd, "23272829");
-	strcpy(db_p->server, "localhost");
-	strcpy(db_p->name, "linux");
-	strcpy(db_p->table, "commands");
+	strcpy(db_p->server, "194.67.204.166");
+	strcpy(db_p->name, "home");
+	strcpy(db_p->table, "dayli_counter");
 	db_p->tc_num = 2;
 
 	//Заполняем наименования колонок таблицы и их тип.
@@ -29,14 +29,14 @@ int main(int argc, char **argv){
 
 	add_to_db(db_p);
 }
-*/
+
 int add_to_db(DBDAT *db){
 	MYSQL *con = mysql_init(NULL);
 	char buffer[1000];
 
 	//Подключаемся к базе (сервер, пользователь, пароль)
 	if (mysql_real_connect(con, db->server, db->user, db->passwd, NULL, 0, NULL, 0) == NULL) {
-//		printf("%s\n", mysql_error(con)); 
+		printf("%s\n", mysql_error(con)); 
 				return -1;
 	}
 
