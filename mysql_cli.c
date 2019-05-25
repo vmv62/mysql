1a6dbf68d2b90ffe8fffcd07aa3d1d03b9440621
vmv <vmv@nxt.ru>
2019-05-25 11:37:24 +0300

    Правка и ошибка

--- mysql_cli.c
+++ mysql_cli.c
@@ -9,7 +9,9 @@ int main(int argc, char **argv){
 
 	DBDAT *db_p;
 	db_p = (DBDAT *)malloc(sizeof(DBDAT));
-
+/*
+Конфигурация подключения должна задаваться из вне
+*/
 	strcpy(db_p->user, "vmv");
 	strcpy(db_p->passwd, "23272829");
 	strcpy(db_p->server, "194.67.204.166");
@@ -17,14 +19,6 @@ int main(int argc, char **argv){
 	strcpy(db_p->table, "dayli_counter");
 	db_p->tc_num = 2;
 
-	//Заполняем наименования колонок таблицы и их тип.
-	strcpy(db_p->col[0].name, "Command");
-	strcpy(db_p->col[0].type, "Tinytext");
-	strcpy(db_p->col[0].data, "\"ls -lsa\"");
-
-	strcpy(db_p->col[1].name, "Note");
-	strcpy(db_p->col[1].type, "Text");
-	strcpy(db_p->col[1].data, "\"Command that print list of files and folders\"");
 
 
 	add_to_db(db_p);
@@ -36,11 +30,11 @@ int add_to_db(DBDAT *db){
 
 	//Подключаемся к базе (сервер, пользователь, пароль)
 	if (mysql_real_connect(con, db->server, db->user, db->passwd, NULL, 0, NULL, 0) == NULL) {
-		printf("%s\n", mysql_error(con)); 
+		perror("%s\n", mysql_error(con)); 
 				return -1;
 	}
 
-	sprintf(buffer, create_db, db->name);
+//	sprintf(buffer, create_db, db->name);
 //	printf("%s\n", buffer);
 
 	//Работа с базой.
@@ -55,11 +49,11 @@ int add_to_db(DBDAT *db){
 
 
 	//Работа с таблицей
-
+/*
 	char tmp[2000];
 	char tmp1[2000];
 	char tmp2[2000];
-
+*/
 	memset(buffer, 0, sizeof(buffer));
 
 	//Для создания таблицы формируем пару: название столбца, тип данных
@@ -70,6 +64,7 @@ int add_to_db(DBDAT *db){
 			strncat(buffer, ", ", strlen(", "));
 		}
 	}
+/*
 	char query_string[2000];
 	//Формируем строку запроса.
 	sprintf(query_string, create_table, db->table, buffer);
@@ -77,7 +72,7 @@ int add_to_db(DBDAT *db){
 	if(mysql_query(con, query_string)){
 //		printf("%s\n", mysql_error(con));
 	}
-
+*/
 
 	//Внесение данных в таблицу
 	memset(buffer, 0, sizeof(buffer));
@@ -105,7 +100,7 @@ int add_to_db(DBDAT *db){
 	sprintf(buffer, insert_to_table, db->table, tmp1, tmp2);
 
 	if(mysql_query(con, buffer)){
-//		printf("%s\n", mysql_error(con));
+		printf("%s\n", mysql_error(con));
 	}
 
 
