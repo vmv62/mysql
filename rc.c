23f2360f31fa85a1b205efaffd5f15ba77e3501c
vmv <vmv@nxt.ru>
2019-06-02 17:45:05 +0300

    Разобраться с strtok

--- rc.c
+++ rc.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "rc.h"
 
 int main(int argc, char **argv){
 	FILE *fd;
 	char buff[BUF_SZ];
+	char *token;
+	const char delim[2] = "=";
+	int param_count = 0;
 
 	fd = fopen("conf", "r");
 	if(fd == NULL){
@@ -12,8 +16,20 @@ int main(int argc, char **argv){
 
 
 	while(fgets(buff, BUF_SZ, fd)){
-		printf("%s", buff);
+		token = strtok(buff, delim);
+		while(token != NULL){
+//			printf("%s", token);
+			strcpy(params[param_count].param_name, token);
+			token = strtok(NULL, delim);
+//			strcpy(params[param_count].param_value, token);
+		}
+			printf("%s", params[param_count].param_name);
+
+//			strcpy(params[param_count].param_value, token);
+			param_count++;
+//			printf("%s\t\t%s\n", params[param_count].param_name, params[param_count].param_value);
 	}
+	printf("Params count: %d\n", param_count);
 
 	fclose(fd);
 
--- rc.h
+++ rc.h
@@ -1,1 +1,8 @@
 #define BUF_SZ	200
+
+typedef struct{
+	char param_name[20];
+	char param_value[20];
+}param_t;
+
+param_t params[20];
